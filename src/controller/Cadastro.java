c8ce9aab06f519a015b7d81e61b95be24c5cd6bc
Pedro-V8 <pedrophdv@outlook.com>
2021-04-16 18:59:24 -0300

    Comentando Classe Cadastro

--- src/controller/Cadastro.java
+++ src/controller/Cadastro.java
@@ -3,6 +3,7 @@ import model.*;
 
 public class Cadastro {
 	
+	//Método para criar Usuário e Salvar no Banco
 	public void criaUsuario(String n , String i , String u , String s , BancoUsuarios banco) {
 		Usuario usuario = new Usuario(n , i , u , s);
 		banco.cadastraUsuario(usuario);

640dc67b74375339a523b77b2194a7fd73ae8545
Pedro-V8 <pedrophdv@outlook.com>
2021-04-16 15:49:29 -0300

    Comentando Classe ListaTarefas

--- src/view/ListaTarefas.java
+++ src/view/ListaTarefas.java
@@ -10,26 +10,26 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class ListaTarefas extends JFrame {
-	
+
+	//Construtor que rece User e Inicia método listaTarefas
 	public ListaTarefas(Usuario user) {
 		listaTarefas(user);
 
 	}
 
-	//private JPanel contentPane;
-	/**
-	 * Create the frame.
-	 */
+
 	public void listaTarefas(Usuario user) {
+		//Criando componentes
 		setTitle("Lista");
 		setSize(335 , 210);
 		setLocationRelativeTo(null);
 		setVisible(true);
-		String[] paises =user.retornaTarefa();
-		JList lista = new JList(paises);
+		String[] tarefas =user.retornaTarefa(); //Adiconando as tarefas salvas no array
+		JList lista = new JList(tarefas);
 		lista.setLocation(50, 0);
 		getContentPane().add(BorderLayout.CENTER , lista);
 		
+		//Criando ação que ao clicar na tarefa, abre o temporizador
 		lista.addListSelectionListener(e -> {
 			if(!e.getValueIsAdjusting()) {
 				Cronometro cron = new Cronometro();
@@ -37,12 +37,6 @@ public class ListaTarefas extends JFrame {
 			}
 			//user.testaTarefa(lista.getSelectedIndex());
 		});
-	}
-	
-	public JList criaLista(Usuario user) {
-
-		JList lista = new JList( user.retornaTarefa());
-		return lista;
 	}
 
 }

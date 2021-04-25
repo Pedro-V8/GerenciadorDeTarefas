ead05f3d79c4dfca04dab2355b4b5350d80ef6d5
Pedro-V8 <pedrophdv@outlook.com>
2021-04-24 21:03:48 -0300

    Adicionando Pasta de Imagem

---
+++ src/testes/TestaUser.java
@@ -0,0 +1,30 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import controller.BancoUsuarios;
+import controller.VerificaDados;
+import model.Usuario;
+
+class TestaUser {
+
+	Usuario usuario = new Usuario("Pedro" , "19" , "PedroV9" , "123");
+	BancoUsuarios banco = new BancoUsuarios();
+	VerificaDados verificaDados = new VerificaDados();
+	
+	@Test
+	void testCadastraUsuario() {
+		assertTrue(verificaDados.verifica("Pedro", "20", "PedroV10", "123123"));
+	}
+
+	@Test
+	void testAutenticaUsuario() {
+		banco.cadastraUsuario(usuario);
+		assertTrue(banco.autenticaUsuario(usuario.getUsuario(), usuario.getSenha()));
+	}
+	
+	
+
+}

074fe5e24fc5cea4649e407e5518fe7d2a973b27
Pedro-V8 <pedrophdv@outlook.com>
2021-04-27 15:00:35 -0300

    Atualizando JavaDoc

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"model","l":"Tarefa"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"model","l":"Usuario"}];updateSearchResults();

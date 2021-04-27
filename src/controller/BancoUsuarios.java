a2b9575a76d7cc92f70bb63a8dfcb0b8b50a1113
Pedro-V8 <pedrophdv@outlook.com>
2021-04-27 17:09:45 -0300

    Documentando Classe BancoUsuarios

--- src/controller/BancoUsuarios.java
+++ src/controller/BancoUsuarios.java
@@ -2,7 +2,15 @@ package controller;
 import java.util.*;
 
 import model.Usuario;
+
+/**
+ * Classe responsável por simular um banco de dados, podendo registrar e retornando o usuário registrado.
+ * @author Pedro Henrique
+ * @version 1.0
+ */
+
 public class BancoUsuarios {
+	
 	
 	private static Usuario user; //Declarando objeto do tipo Usuario
 	ArrayList<Usuario> usuarios = new ArrayList(); //ArrayList para salvar os Usuários Registrados
@@ -13,7 +21,12 @@ public class BancoUsuarios {
 
 	}
 	
-	//Método para verificar se o Usuário está cadastrado, retornando 1 para sim e 0 para não.
+	/**
+	 * Método responsável por verificar se o usuário digitado está cadastrado
+	 * @param usuario
+	 * @param senha
+	 * @return boolean, true para cadastrado e false para não cadastrado
+	 */
 	public boolean autenticaUsuario(String usuario , String senha) {
 		boolean resposta = false;
 		for(Usuario userTeste: usuarios) {

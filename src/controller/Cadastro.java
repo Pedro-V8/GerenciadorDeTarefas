23fc7bd55a2a8e6f48cec2d5ef4816803265806f
Pedro-V8 <pedrophdv@outlook.com>
2021-04-27 17:25:38 -0300

    Documentando Classe Cadastro

--- src/controller/Cadastro.java
+++ src/controller/Cadastro.java
@@ -1,9 +1,22 @@
 package controller;
 import model.*;
 
+/**
+ * Classe responsável por criar Objeto Usuários
+ * @author Pedro Henrique
+ * @version 1.0
+ */
 public class Cadastro {
 	
-	//Método para criar Usuário e Salvar no Banco
+	/**
+	 * Método responsável por pegar os dados digitados e criar o objeto Usuário
+	 * @param n , nome
+	 * @param i , idade
+	 * @param u , usuario
+	 * @param s , senha
+	 * @param banco , Banco de Dados para salvar
+	 * @return nenhum
+	 */
 	public void criaUsuario(String n , String i , String u , String s , BancoUsuarios banco) {
 		Usuario usuario = new Usuario(n , i , u , s);
 		banco.cadastraUsuario(usuario);

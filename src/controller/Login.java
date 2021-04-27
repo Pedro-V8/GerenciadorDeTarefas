997d92f6a091fb820ffe8e6f10464c41991a73d7
Pedro-V8 <pedrophdv@outlook.com>
2021-04-27 12:33:33 -0300

    Comentando Classe Login

--- src/controller/Login.java
+++ src/controller/Login.java
@@ -7,7 +7,7 @@ public class Login {
 	// Método que verifica se o Usuario está no Banco, caso 1 (Sim), ele entra no sistema
 	public boolean mostraUsuario(BancoUsuarios banco , String usuario , String senha) {
 		boolean entrar = false;
-		if(banco.autenticaUsuario(usuario , senha) == 1) {
+		if(banco.autenticaUsuario(usuario , senha) == true) {
 			entrar = true;
 		}else {
 			entrar = false;

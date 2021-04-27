251f0a165178edd2adc0dc414531c3149573d995
Pedro-V8 <pedrophdv@outlook.com>
2021-04-27 18:29:04 -0300

    Atualizando JavaDoc

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"controller","l":"Cadastro"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"controller","l":"VerificaDados"}];updateSearchResults();

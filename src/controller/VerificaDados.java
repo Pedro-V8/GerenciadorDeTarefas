bc75865bbaf4cdb6f2daaaabe9d8a6101afb22ed
Pedro-V8 <pedrophdv@outlook.com>
2021-04-27 23:32:28 -0300

    Documentando Classe VerificaDados

--- src/controller/VerificaDados.java
+++ src/controller/VerificaDados.java
@@ -13,7 +13,7 @@ public class VerificaDados {
 	 * @param idade
 	 * @param user
 	 * @param senha
-	 * @return booleano, onde true representa que o usuário digitou corretamente e false representa que o usuário digitou algo errado.
+	 * @return booleano, onde true representa que o usuário digitou corretamente e false que o usuário digitou algo errado.
 	 */
 	public boolean verifica(String nome , String idade , String user , String senha) {
 		boolean resposta = true;
